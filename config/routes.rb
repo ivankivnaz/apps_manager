16c76599712b97b82c7fde0616b1d24f9d3ff12f
ivankivnaz <ivankivnaz@gmail.com>
2019-09-22 15:19:03 +0300

    - default page; seed web_apps data

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root to: "web_applications#index"
+
   resources :web_applications
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
